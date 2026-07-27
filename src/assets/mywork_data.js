5e4016aa2d1cd09eda56f0375cd4bd4297e6ee08
anoopyadav-01 <anoopyadav7977195135@gmail.com>
2026-07-27 19:29:10 +0530

    Add files via upload

---
+++ src/assets/mywork_data.js
@@ -0,0 +1,33 @@
+import project1_img from "../assets/Screenshot 2026-06-12 151209.png";
+import project2_img from "../assets/FeastFlow.png";
+import project3_img from "../assets/Screenshot 2026-07-17 172957.png";
+import project4_img from "../assets/Gradeproj.png";
+
+const mywork_data = [
+  {
+    w_no: 1,
+    w_name: "AdvocateHub",
+    w_img: project1_img,
+    w_link: "https://github.com/anoopyadav-01/AdvocateHub.git",
+  },
+  {
+    w_no: 2,
+    w_name: "FeastFlow",
+    w_img: project2_img,
+    w_link: "https://github.com/anoopyadav-01/FeastFlow.git",
+  },
+  {
+    w_no: 3,
+    w_name: "PortFolio",
+    w_img: project3_img,
+    w_link: "https://github.com/anoopyadav-01/FeastFlow.git",
+  },
+  {
+    w_no: 4,
+    w_name: "Grade Analytics Suite",
+    w_img: project4_img,
+    w_link: "https://github.com/anoopyadav-01/FeastFlow.git",
+  },
+];
+
+export default mywork_data;
---
+++ src/assets/services_data.js
@@ -0,0 +1,28 @@
+const Services_Data = [
+  {
+    s_no: "01",
+    s_name: "AdvocateHub",
+    s_img: project1_img,
+    s_link: "https://github.com/anoopyadav-01/AdvocateHub.git",
+  },
+  {
+    s_no: "02",
+    s_name: "FeastFlow",
+    s_img: project1_img,
+    s_link: "https://github.com/anoopyadav-01/FeastFlow.git",
+  },
+  {
+    s_no: "03",
+    s_name: "PortFolio",
+    s_img: project1_img,
+    s_link: "https://github.com/anoopyadav-01/FeastFlow.git",
+  },
+  {
+    s_no: "04",
+    s_name: "Grade Analytics Suite",
+    s_img: project1_img,
+    s_link: "https://github.com/anoopyadav-01/FeastFlow.git",
+  },
+];
+
+export default Services_Data;
